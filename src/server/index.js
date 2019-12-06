fc4c142130029b0f1f37a485e42684ee2cf5db92
mrcreel <michaelrcreel@gmail.com>
2019-12-06 10:57:57 -0600

    Init commit

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(helmet())
+app.use(cors())
+
+const PORT = process.env.PORT || 1337
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+
+console.log('app')
