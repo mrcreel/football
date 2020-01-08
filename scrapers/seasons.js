563528d657a0ba5847c89f7c1d9b3db1de5f6788
mrcreel <michaelrcreel@gmail.com>
2020-01-08 12:38:55 -0600

    Try better way to scrape seasons

--- scrapers/seasons.js
+++ scrapers/seasons.js
@@ -1,91 +1,44 @@
-const { MongoClient } = require('mongodb')
-require('dotenv').config()
-
 const axios = require('axios')
 const cheerio = require('cheerio')
+
+const fs = require('fs')
+
+require('dotenv').config()
 
 function cleanText(string) {
   return string.replace(/(\r\n|\n|\r) /gm, '')
 }
 
-const seasons = async () => {
-  console.log('seasons.js')
-
-  /** TODO: Read teams collection on mongoDB */
-
-  const client = new MongoClient(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-
-  try {
-    // Connect to the MongoDB cluster
-    const teamsArray = []
-    await client.connect()
-
-    client
-      .db('football')
-      .collection('seasons')
-      .createIndex({ teamId: 1, teamSeason: 1 }, { unique: true })
-
-    const result = await client
-      .db('football')
-      .collection('teams')
-      .find({ inState: true })
-
-    /** Read each from 'teams' collection */
-
-    result.forEach(async doc => {
-      const teamId = doc.teamId
-      const page = `http://misshsfootball.com/Teams/${teamId}_Standings.htm`
-
-      const response = await axios.get(page)
-      const body = await response.data
-
-      const $ = await cheerio.load(body)
-      const data = $(`tr`)
-
-      let $data = $(data)
-
-      $data = $data.slice(8, $data.length - 5)
-
-      const pageData = []
-
-      // console.log(`${teamId}-> ${page} -> ${$data.length}`)
-
-      let ct = 1
-      $data.each(async (index, element) => {
-        const rowData = $(element).children()
-        const teamSeason = parseInt($(rowData[1]).text())
-        if (!isNaN(teamSeason)) {
-          const teamRegion = cleanText($(rowData[2]).text())
-          const splitClass = teamRegion.split(/[\s-]+/)
-          const teamClass =
-            splitClass.length == 1 ? splitClass[0] : splitClass[1]
-          // console.log(`${ct}) ${teamSeason}: ${teamClass} | ${teamRegion}`)
-
-          const season = await client
-            .db('football')
-            .collection('seasons')
-            .insertOne({
-              teamId,
-              teamSeason,
-              teamRegion,
-              teamClass
-            })
-          console.log(
-            `${ct}) ${teamId} Season ${teamSeason} created with id: ${season.insertedId}`
-          )
-
-          ct++
-        }
-      })
-    })
-  } catch (err) {
-    console.log(err)
-  } finally {
-    await client.close()
-  }
+function padNumber(number) {
+  return number >= 100 ? number : number < 10 ? `00${number}` : `0${number}`
 }
 
-seasons()
+const writeJson = data => {
+  fs.writeFile('./scrapers/data/seasons.json', JSON.stringify(data), err => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Written')
+    }
+  })
+}
+let c = 1
+const teams = require('./data/teams.json')
+const scrapePage = async () => {
+  let teamCount = 0
+  teams.forEach(async team => {
+    teamCount += 1
+    const teamId = team.teamId
+    const countDisplay = padNumber(teamCount)
+
+    const page = `http://misshsfootball.com/Teams/${teamId}_Standings.htm`
+
+    const response = await axios.get(page)
+    console.log(
+      `${padNumber(c)}: ${countDisplay})  ${teamId} -> ${response.status}`
+    )
+    c += 1
+  })
+}
+
+scrapePage()
---
+++ scrapers/teams.js
@@ -0,0 +1,68 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const fs = require('fs')
+
+require('dotenv').config()
+
+function cleanText(string) {
+  return string.replace(/(\r\n|\n|\r) /gm, '')
+}
+
+const pages = [
+  'http://misshsfootball.com/Teams/index.htm',
+  'http://misshsfootball.com/Teams/Inactive.htm'
+]
+
+const scrapePages = async pages => {
+  const requests = pages.map(async page => {
+    /** Read the page with Axios */
+    const response = await axios.get(page)
+    const body = await response.data
+
+    /** Load page with Cheerio */
+    const $ = await cheerio.load(body)
+
+    /** Create array of data elements */
+    const scrapedElements = $('td[width=152] a')
+
+    const pageRequestData = []
+    scrapedElements.each((idx, item) => {
+      const teamId = $(item)
+        .attr('href')
+        .split('.')[0]
+      const teamName = cleanText($(item).text())
+      const team = {
+        teamId,
+        teamName,
+        inState: true
+      }
+
+      pageRequestData.push(team)
+    })
+
+    return await pageRequestData
+  })
+
+  const allRequestsData = await axios.all(requests)
+  // TODO: flatten allRequestsData
+  const allData = []
+  allRequestsData.forEach(pageData => {
+    pageData.forEach((item, idx) => {
+      allData.push(item)
+    })
+  })
+
+  writeJson(allData)
+}
+
+const writeJson = data => {
+  fs.writeFile('./scrapers/data/teams.json', JSON.stringify(data), err => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Written')
+    }
+  })
+}
+scrapePages(pages)
