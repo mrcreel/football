7310052a3d3b0f5d985a323cfd2399227e55751b
Michael R Creel <michaelrcreel@gmail.com>
2024-01-12 05:03:12 -0600

    Add test text to <main>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <main className='flex flex-col w-100 border-solid border-2 border-black bg-slate-200 p-2 m-2'>
-        Main
+        This is a test
       </main>
       <footer className='flex flex-col w-100 border-solid border-2 border-black bg-slate-200 p-2 m-2'>
         footer
