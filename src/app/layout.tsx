247de0f1bef5fc873e9b7db192fb8fd45f6a5c2c
Michael R Creel <michaelrcreel@gmail.com>
2024-01-12 05:17:27 -0600

    Add Vercel Analytics to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Header from "./components/Header"
 import "./globals.css"
 
@@ -19,6 +20,7 @@ export default function RootLayout({
         <Header />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
