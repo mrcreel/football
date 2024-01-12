e50f53bd346497fee58c5fb98863614ab6ca8c8a
Michael R Creel <michaelrcreel@gmail.com>
2024-01-12 07:36:49 -0600

    Initialize Prisma

---
+++ src/app/data/Division.ts
@@ -0,0 +1,21 @@
+export const divisions = [
+  {
+    name: "Division I",
+    conferences: [
+      { confId: "654ee0c4780a25459622df9f" },
+      { confId: "654ee0c4780a25459622dfa6" },
+      { confId: "654ee0c4780a25459622dfb1" },
+      { confId: "654ee0c4780a25459622dfc7" },
+      { confId: "654ee0c4780a25459622dfd8" },
+      { confId: "654ee0c4780a25459622dfd8" },
+      { confId: "654ee0c4780a25459622dfe3" },
+    ],
+  },
+  {
+    name: "Division III",
+    conferences: [
+      { id: "654ee0c4780a25459622e01e" },
+      { id: "654ee0c4780a25459622e02c" },
+    ],
+  },
+]
