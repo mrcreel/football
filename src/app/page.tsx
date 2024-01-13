d2359570ba200ec8dd66ee2dac2ff76c326b6bdb
Michael R Creel <michaelrcreel@gmail.com>
2024-01-12 22:07:53 -0600

    Graphql codegen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-"use client"
-
-import Link from "next/link"
-import React, { useState } from "react"
-
-import Header from "./components/Header"
-
 export default function Home() {
   return (
     <>
-      <main className='flex flex-col w-100 border-solid border-2 border-black bg-slate-200 p-2 m-2'>
+      <main className='flex flex-col flex-grow w-100 border-solid border-2 border-black bg-slate-200 p-2 m-2'>
         Main
       </main>
       <footer className='flex flex-col w-100 border-solid border-2 border-black bg-slate-200 p-2 m-2'>
